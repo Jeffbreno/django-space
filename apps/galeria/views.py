f874a52a3e4217c5e28e63c7b4f15a9107587b47
Jefferson Lopes <jeffbreno@gmail.com>
2024-05-03 02:06:58 -0300

    03052024_0206

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import FotoGrafia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 
@@ -20,7 +21,7 @@ def buscar(request):
     if not request.user.is_authenticated:
         messages.error(request, "Usuário não logado")
         return redirect("login")
-    
+
     fotografias = FotoGrafia.objects.order_by("data_fotografia").filter(publicada=True)
     if "buscar" in request.GET:
         nome_a_busca = request.GET["buscar"]
@@ -28,3 +29,42 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_busca)
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
+
+
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect("login")
+
+    form = FotografiaForms
+    if request.method == "POST":
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Nova fotografia cadastrada!")
+            return redirect("index")
+
+    return render(request, "galeria/nova_imagem.html", {"form": form})
+
+
+def editar_imagem(request, foto_id):
+    fotografia = FotoGrafia.objects.get(id=foto_id)
+    form = FotografiaForms(instance=fotografia)
+
+    if request.method == "POST":
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Fotografia editada com sucesso!")
+            return redirect("index")
+
+    return render(
+        request, "galeria/editar_imagem.html", {"form": form, "foto_id": foto_id}
+    )
+
+
+def deletar_imagem(request, foto_id):
+    fotografia = FotoGrafia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, "Fotografia deletada com sucesso!")
+    return redirect("index")
